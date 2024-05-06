2957338c8360f9c0184898f8f801272a8f66206f
Esral Desta <esraldesta@gmail.com>
2024-05-06 07:15:50 -0300

    Add project files.

---
+++ Estore.Services.CouponAPI/Data/AppDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Estore.Services.CouponAPI.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Estore.Services.CouponAPI.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { 
+            
+        }
+
+        public DbSet<Coupon> Coupons { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 1,
+                CouponCode="sdjfls",
+                DiscountAmount = 20,
+                MinAmount=40
+            }            
+            );
+
+            modelBuilder.Entity<Coupon>().HasData(new Coupon
+            {
+                CouponId = 2,
+                CouponCode = "fnskd",
+                DiscountAmount = 10,
+                MinAmount = 50
+            }
+);
+        }
+    }
+}
