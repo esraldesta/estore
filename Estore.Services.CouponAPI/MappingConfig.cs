1fbb0c28d577572bfadbe3874fc9c713bc3fb384
Esral Desta <esraldesta@gmail.com>
2024-05-07 05:20:25 -0300

    CouponAPI basic CRUD

---
+++ Estore.Services.CouponAPI/MappingConfig.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Estore.Services.CouponAPI.Models;
+using Estore.Services.CouponAPI.Models.Dto;
+
+namespace Estore.Services.CouponAPI
+{
+    public class MappingConfig
+    {
+        public static MapperConfiguration RegisterMaps() { 
+            var mappingConfig = new MapperConfiguration(config => 
+            {
+                config.CreateMap<CouponDto, Coupon>();
+                config.CreateMap<Coupon, CouponDto>();
+            });
+            return mappingConfig;
+        }
+    }
+}
---
+++ Estore.Services.CouponAPI/Models/Dto/ResponseDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Estore.Services.CouponAPI.Models.Dto
+{
+    public class ResponseDto
+    {
+        public object? Result { get; set; }
+        public bool IsSuccess {  get; set; } = true;
+        public string Message { get; set; } = string.Empty;
+
+    }
+}
